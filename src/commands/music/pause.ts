f28f45b537226db650f87c0d34d4386ce9ab374d
OuOSama <real.ouosama@gmail.com>
2025-08-23 22:48:37 +0700

    feat(Music): add pause function

---
+++ src/commands/music/pause.ts
@@ -0,0 +1,17 @@
+// src/commands/music/pause.ts
+
+import { Command, type CommandContext, Declare } from 'seyfert'
+import { client } from 'src/app'
+
+@Declare({
+	name: 'pause',
+	description: 'pause current song',
+})
+export default class PauseMusic extends Command {
+	async run(ctx: CommandContext) {
+		// Player
+		const player = client.riffy.get(ctx.guildId as string)
+		player.pause(true)
+		await ctx.write({ content: 'Song has pause' })
+	}
+}
