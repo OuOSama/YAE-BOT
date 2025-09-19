63751370f13e8a6ce3ae87b13f43c2da9f1cef53
OuOSama <real.ouosama@gmail.com>
2025-09-19 21:54:52 +0700

    🔝 feat(music): improve resume command with checks and user feedback

--- src/commands/music/resume.ts
+++ src/commands/music/resume.ts
@@ -1,17 +1,33 @@
 // src/commands/music/resume.ts
 
 import { Command, type CommandContext, Declare } from 'seyfert'
+import { MessageFlags } from 'seyfert/lib/types'
 import { client } from 'src/app'
 
 @Declare({
 	name: 'resume',
-	description: 'resume current song',
+	description: '⏯️ Resume the current song',
 })
-export default class PauseMusic extends Command {
+export default class ResumeMusic extends Command {
 	async run(ctx: CommandContext) {
 		// Player
 		const player = client.riffy.get(ctx.guildId as string)
-		player.pause(false)
-		await ctx.write({ content: 'Song has pause' })
+
+		if (!player) return
+		ctx.write({ content: '❌ No active player!' })
+
+		// is paused?
+		if (!player.playing) {
+			player.pause(false) // resume
+			await ctx.write({
+				content: '▶️ Song resumed!',
+				flags: MessageFlags.Ephemeral,
+			})
+		} else {
+			await ctx.write({
+				content: 'eh... Song is already playing',
+				flags: MessageFlags.Ephemeral,
+			})
+		}
 	}
 }
