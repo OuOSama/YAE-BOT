c4276e83c41804a5551c5ebdb0a46637f5707c6e
OuOSama <real.ouosama@gmail.com>
2025-11-08 20:51:10 +0700

    🐛 fix(queueEnd.ts) : fix wrong event name

--- src/events/music/queueEnd.ts
+++ src/events/music/queueEnd.ts
@@ -2,7 +2,7 @@
 
 import { client } from 'src/app'
 
-client.riffy.on('nodeConnect', (play) => {
+client.riffy.on('queueEnd', (play) => {
 	// exit VC room when queue has end / no song to play anymore
 	play.disconnect()
 	client.logger.debug(`${client.me.name} has exit the VC Room`)
