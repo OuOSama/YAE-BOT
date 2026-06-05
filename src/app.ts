294ce1ac7b8567963e0ce28942bbf2bb3483d0c7
OuOSama <real.ouosama@gmail.com>
2026-06-05 20:36:00 +0700

    ✨ feat (app) : add prefix command

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
 // src/app.ts
 
 import { Client } from 'seyfert'
-import createKazagumo from './managers/createKazagumo'
+import createKazagumo from '@/managers/createKazagumo.js'
 
-const client = new Client()
+const client = new Client({
+	commands: {
+		prefix: () => {
+			// here you can handle whatever prefixes you want depending on the message data.
+			return ['!', '?', '.']
+		},
+	},
+})
 
 //Basic configuration, perfect for this case
 client.kazagumo = createKazagumo()
